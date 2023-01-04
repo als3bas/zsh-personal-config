94b287e1cad8e2c7a936b28f3be32e630c2dc2f3
Álvaro Sebastián <sebalvaro@users.noreply.github.com>
2023-01-04 15:04:47 -0300

    🔥 Goodbye zplug , welcome zinit uwu

--- install.zsh
+++ install.zsh
@@ -2,9 +2,9 @@
 echo "📂 Changing directory to ~"
 cd ~
 
-# install zplug
-echo "📦 Installing zplug"
-curl -sL --proto-redir -all,https "https://raw.githubusercontent.com/zplug/installer/master/installer.zsh?$RANDOM" | zsh
+# install zinit
+echo "📦 Installing Zinit"
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 # download .zshrc & configs
 echo "📥 Downloading zsh assets"
